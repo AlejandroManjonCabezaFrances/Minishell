08a05c5b7f5fcc3637f4b26f6651bc7b51881c2d
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-01-18 13:13:49 +0100

    poltergeist echo casi hecho

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,43 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 09:13:09 by amanjon-          #+#    #+#             */
-/*   Updated: 2024/01/17 14:35:06 by marvin           ###   ########.fr       */
+/*   Updated: 2024/01/18 13:13:18 by amanjon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// gcc -Wall -Werror -Wextra ../utils.c builtins.c echo.c -o echo && ./echo
+// gcc -Wall -Werror -Wextra ../../libft/Libft/src/ft_putstr_fd.c ../utils.c builtins.c pwd.c echo.c -o echo && ./echo
 
-static void    ft_epur_str(char *str)
+// static void    ft_epur_str(char *str)
+// {
+// 	int i;
+// 	int j;
+// 	int k;
+
+// 	i = 0;
+// 	j = 0;
+// 	k = 0;
+// 	while(str[k] != '\0')
+// 		k++;
+// 	k--;
+// 	while(str[k] == ' ' || str[k] == '\t')
+// 		k--;
+// 	while(str[j] == ' ' || str[j] == '\t')
+// 		j++;
+// 	while(str[i] != '\0' && i <=k)
+// 	{
+// 		while(str[i] == ' ' || str[i] == '\t')
+// 			i++;
+// 		if(str[i - 1] == ' ' && i > j)
+// 			write(1, " ", 1);
+// 		write(1, &str[i], 1);
+// 		i++;
+// 	}
+// }
+
+static void	ft_writing_echo(char **cmd, int len)
 {
-	int i;
-	int j;
-	int k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	while(str[k] != '\0')
-		k++;
-	k--;
-	while(str[k] == ' ' || str[k] == '\t')
-		k--;
-	while(str[j] == ' ' || str[j] == '\t')
-		j++;
-	while(str[i] != '\0' && i <=k)
+	printf("cmd[1] = %s\n", cmd[1]);
+	while (cmd[len])
 	{
-		while(str[i] == ' ' || str[i] == '\t')
-			i++;
-		if(str[i - 1] == ' ' && i > j)
-			write(1, " ", 1);
-		write(1, &str[i], 1);
-		i++;
+		ft_putstr_fd(cmd[len], 1);
+		// if (cmd[len])
+		// write(1, " ", 1);
+		len++;
 	}
-}
+}	
 
 static int	ft_flag_n(char *str, int *len)
 {	
@@ -64,28 +76,31 @@ void	ft_echo(char **cmd)
 	}
 	if (ft_flag_n(cmd[len], &len) == TRUE)
 		flag = 1;
-	while (cmd[len])
-	{
-		ft_epur_str(cmd[len]);
-		len++;
-	}
+	// while (cmd[len])
+	// {
+		// ft_epur_str(cmd[len]);
+		ft_writing_echo(cmd, len);
+	// 	printf("\nlen = %d\n", len);
+	// 	len++;
+	// }
 	if (flag == TRUE)
 		write(1, "\n", 1);
 }
 
 int main() 
 {
-    char *cmd[] = {"echo","-n", "     hola          diana   ", NULL};
-    char **cmdPtr = cmd;
+    // char *cmd[] = {"echo",/* "-n", */ "hola diana" "alex que tal", NULL};
+    // char **cmdPtr = cmd;
 	
-    ft_builtins(cmdPtr);
+    // ft_builtins(cmdPtr);
 
-	// char *cmd[3];
+	char *cmd[4];
 
-	// cmd[0] = "echo";
-	// cmd[1] = "lo que diga el papi";
-	// cmd[2] = NULL;
+	cmd[0] = "echo";
+	cmd[1] = "lo que diga el papi";
+	cmd[2] = "alex que tal";
+	cmd[3] = NULL;
 
-	// ft_builtins(cmd);
+	ft_builtins(cmd);
     return (0);
 }
--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -12,25 +12,38 @@
 
 #include "../../include/minishell.h"
 
-// gcc -Wall -Werror -Wextra ../utils.c builtins.c pwd.c -o pwd && ./pwd
+// gcc -Wall -Werror -Wextra ../../libft/Libft/src/ft_putstr_fd.c ../utils.c builtins.c echo.c pwd.c -o pwd && ./pwd
+// bash pwd < builtins.c 
 
 void    ft_pwd(char **cmd)
-{
+{	
+    char cwd[PATH_MAX];
 
+    if (cmd[1] != NULL && cmd[1][0] != '<' && cmd[1][0] != '>')
+	{
+		ft_putstr_fd("pwd without arguments according to subject\n", 2);
+		return ;
+	}
+	else 
+	{
+		getcwd(cwd, sizeof(cwd));
+        printf("%s\n", cwd); 
+    }
+    return ;
 }
 
-int main() 
-{
-    char *cmd[] = {"pwd", NULL};
-    char **cmdPtr = cmd;
+// int main() 
+// {
+//     char *cmd[] = {"pwd",/*  " <  builtins.c", */ NULL};
+//     char **cmdPtr = cmd;
 	
-    ft_builtins(cmdPtr);
+//     ft_builtins(cmdPtr);
 
-	// char *cmd[3];
+// 	// char *cmd[3];
 
-	// cmd[0] = "pwd";
-	// cmd[1] = NULL;
+// 	// cmd[0] = "pwd";
+// 	// cmd[1] = NULL;
 
-	// ft_builtins(cmd);
-    return (0);
-}
+// 	// ft_builtins(cmd);
+//     return (0);
+// }
