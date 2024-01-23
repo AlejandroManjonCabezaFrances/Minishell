0f7bebfb77b37c319b028b25506c8a72e6126d73
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-01-23 15:56:37 +0100

    export bubble sort no hecho

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 11:41:16 by amanjon-          #+#    #+#             */
-/*   Updated: 2024/01/19 11:31:05 by marvin           ###   ########.fr       */
+/*   Updated: 2024/01/23 15:09:37 by amanjon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	ft_builtins(char **cmd, char **env)
+void	ft_builtins(char **cmd, char **env_cpy)
 {
 	if (ft_strcmp(*cmd, "echo") == 0)
 		ft_echo(cmd);
 	else if (ft_strcmp(*cmd, "pwd") == 0)
 		ft_pwd(cmd);
 	else if (ft_strcmp(*cmd, "export") == 0)
-		ft_export(cmd, env);
+		ft_export(cmd, env_cpy);
 	// else if (ft_strcmp(*cmd, "cd") == 0)
 	// 	ft_cd(cmd);
 	// else if (ft_strcmp(*cmd, "unset") == 0)
--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 09:13:14 by amanjon-          #+#    #+#             */
-/*   Updated: 2024/01/22 17:02:34 by marvin           ###   ########.fr       */
+/*   Updated: 2024/01/23 15:40:07 by amanjon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,47 +40,104 @@ void ft_print_lst_2(t_env *temp)
 	}
 }
 
-void	ft_export_with_flag(char **cmd, char **env)
+void	ft_export_with_flag(char *cmd, char **env_cpy)
 {
     t_env *envi;
-    ft_linked_list_env(&envi, env);
+	
+	envi = NULL;
+    ft_linked_list_env(&envi, env_cpy);
     // ft_print_lst_2(envi);
-	ft_lstadd_penultimate_str_env(&envi, ft_lstnew_penultimate_str_env(cmd[1]));
+	ft_lstadd_penultimate_str_env(&envi, ft_lstnew_penultimate_str_env(cmd));
 	ft_print_lst_2(envi);
 }
 
-void    ft_export(char **cmd, char **env)
+void	ft_export_without_argv(char **env_cpy)
 {
-	int j;
-	int fail;
+		
+	
+}
+
+void    ft_export(char **cmd, char **env_cpy)
+{
+	int		j;
+	int		fail;
+	int		k;
+	char	*aux; 
 
 	j = 1;
 	fail = 0;
+	aux = NULL;
+	aux = malloc(sizeof(char) * (ft_strlen(cmd[1]) + 1));
+	if (aux == NULL)
+		return ;
+	if (cmd[1] == NULL)
+		ft_export_without_argv(env_cpy);
 	while (cmd[1][j])
 	{
-		if ((cmd[1][j] == '=' && ((cmd[1][j - 1] >= 'a' && cmd[1][j - 1] <= 'z')  // 1=alex , no es correcto numeros
-				|| (cmd[1][j - 1] >= 'A' && cmd[1][j - 1] <= 'Z'))))
+		if ((cmd[1][j] == '=' && ft_isalpha(cmd[1][j - 1])))
 		{
-			if (ft_strchr(cmd))// intentar encontrar el = + 1, y star and end ara ver largo del string sin ' ' o primer caracter del string ' ' ok
-				ft_export_with_flag(cmd, env);
-				fail = 1;
+			fail = 1;
+			break;
 		}
 		j++;
 	}
+	while (cmd[1][j] != ' ' && j >= 0)
+		j--;
+	j++;
+	k = 0;
+	while (cmd[1][j] != ' ' && cmd[1][j])
+	{
+		aux[k] = cmd[1][j];
+		k++;	
+		j++;
+	}
+	aux[k] = '\0';
 	if (fail == 0)
-		printf("arguments not found");
+		printf("arguments not founds");
+	else
+		ft_export_with_flag(aux, env_cpy);
+}
+// copia del env (esta función habrá que quitarla, está en el main)
+char	**copy_env(char **env)
+{
+	char	**env_cpy;
+	int		i;
+
+	i = 0;
+	while (env[i])
+		i++;
+	env_cpy = (char **)malloc(sizeof(char *) * (i + 1));
+	if (!env_cpy)
+		return (NULL);
+	i = 0;
+	while (env[i])
+	{
+		env_cpy[i] = ft_strdup(env[i]);
+		i++;
+	}
+	env_cpy[i] = NULL;
+	return (env_cpy);
 }
 
 int main(int argc, char **argv, char **env) 
 {
 	char *cmd[3];
+	char	**env_cpy;
     (void) argc;
     (void) argv;
     
+	env_cpy = NULL;
+	env_cpy = copy_env(env);
 	cmd[0] = "export";
-	cmd[1] = "ALEX=alex";
+	// cmd[1] = "A LEX=alex";
+	cmd[1] = "LEX= alex";
+	// cmd[1] = "ALEX=alex";
+	// cmd[1] = NULL;
 	cmd[2] = NULL;
-	ft_builtins(cmd, env);
+	ft_builtins(cmd, env_cpy);
 
+	// habria que probar que ft_export devuelva un doble puntero
+	// y guardar la lista --> env_cpy = char **ft_export(). 
+	// antes de esto, hacer free a env_cpy por los leaks.
     return (0);
 }
